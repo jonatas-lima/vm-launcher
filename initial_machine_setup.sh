3a8818acf083442f54e2722be15822b416a374c9
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-12-01 14:23:05 -0300

    refactoring

--- initial_machine_setup.sh
+++ initial_machine_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-GIT_BRANCH=only_needed
+GIT_BRANCH=master
 
 sudo apt update && sudo apt upgrade -y && sudo apt install -y wget curl iperf3 git
 
 # Iniciando o servidor iperf em modo daemon
 iperf3 -s -D
 
-git clone https://github.com/jonatas-lima/zabbix-agent-config.git -b $GIT_BRANCH && cd zabbix-agent-config
+git clone https://github.com/jonatas-lima/vm-config -b $GIT_BRANCH && cd vm-config
 
-bash './install_and_config_zabbix_agent.sh'
+bash './launch_zabbix_agent.sh'
 bash './launch_mysql_container.sh'
--- scripts/generate_udp_traffic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-HOST=
-TIME=$1
-
-iperf3 -c $HOST -t $TIME -u
