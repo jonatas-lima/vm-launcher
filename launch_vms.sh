8a648b9390f110ef601fcd1d976550610cd55725
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-12-08 12:04:29 -0300

    feat: including OS image to vms file and bug fixing

--- launch_vms.sh
+++ launch_vms.sh
@@ -5,13 +5,14 @@ VMS_FILE='./vms.csv'
 while IFS= read -r line
 do
   HOSTNAME=$(echo $line | cut -d',' -f1)
-  IP=$(echo $line | cut -d',' -f2)
-  NETWORK_INTERFACE=$(echo $line | cut -d',' -f3)
-  MEMORY=$(echo $line | cut -d',' -f4)
-  CPUS=$(echo $line | cut -d',' -f5)
-  STACK=$(echo $line | cut -d',' -f6)
-  ZABBIX_SERVER_IP=$(echo $line | cut -d',' -f7)
+  OS=$(echo $line | cut -d',' -f2)
+  IP=$(echo $line | cut -d',' -f3)
+  NETWORK_INTERFACE=$(echo $line | cut -d',' -f4)
+  MEMORY=$(echo $line | cut -d',' -f5)
+  CPUS=$(echo $line | cut -d',' -f6)
+  STACK=$(echo $line | cut -d',' -f7)
+  ZABBIX_SERVER_IP=$(echo $line | cut -d',' -f8)
 
-  bash './create_vagrantfile.sh' $HOSTNAME $IP $NETWORK_INTERFACE $MEMORY $CPUS $STACK $ZABBIX_SERVER_IP
+  bash './create_vagrantfile.sh' $HOSTNAME $OS $IP $NETWORK_INTERFACE $MEMORY $CPUS $STACK $ZABBIX_SERVER_IP
   cd ./vms/$HOSTNAME && vagrant up && cd ../../
 done < $VMS_FILE
